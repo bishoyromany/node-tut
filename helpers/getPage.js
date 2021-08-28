a353a89e3e3237910921131b8c623e73c3ce667e
Bishoy Romany <7bishoyromany7@gmail.com>
2021-08-28 20:28:23 +0200

    Load Assets Dynamically On Server

--- helpers/getPage.js
+++ helpers/getPage.js
@@ -1,10 +1,56 @@
 const fs = require("fs");
 const path = require("path");
 
-const getPage = (filename) => {
-  return fs.readFileSync(
-    path.join(__dirname, "..", "public", "pages", filename)
-  );
+const htmlHeader = (res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+};
+
+const jsonHeader = (res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+};
+
+const cssHeader = (res) => {
+  res.writeHead(200, { "Content-Type": "text/css" });
+};
+
+const jsHeader = (res) => {
+  res.writeHead(200, { "Content-Type": "application/javascript" });
+};
+
+const headers = (res, ext) => {
+  if (ext === "json") {
+    jsonHeader(res);
+  } else if (ext === "html") {
+    htmlHeader(res);
+  } else if (ext === "css") {
+    cssHeader(res);
+  } else if (ext === "js") {
+    jsHeader(res);
+  }
+};
+
+const getPage = (filename, res) => {
+  let ext = path.extname(filename);
+  if (!ext) {
+    filename += ".html";
+  }
+  ext = path.extname(filename);
+
+  if (ext === ".html") {
+    filename = "pages/" + filename;
+  }
+
+  const fileDir = path.join(__dirname, "..", "public", filename);
+
+  fs.readFile(path.join(fileDir), (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end();
+      return;
+    }
+    headers(res, ext);
+    res.end(data);
+  });
 };
 
 module.exports = getPage;
--- router/urlHandler.js
+++ router/urlHandler.js
@@ -1,22 +1,7 @@
 const getPage = require("./../helpers/getPage");
 
-const htmlHeader = (res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-};
-
 const urlHandler = (req, res) => {
-  let page = null;
-  if (req.url === "/") {
-    page = getPage("index.html");
-  } else if (req.url === "/about") {
-    page = getPage("about.html");
-  }
-  if (!page) {
-    res.writeHead(404);
-  } else {
-    htmlHeader(res);
-  }
-  res.end(page);
+  getPage(req.url === "/" ? "index.html" : req.url, res);
 };
 
 module.exports = urlHandler;

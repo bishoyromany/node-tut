3597a8a8835010e7fb99430c6c0e3e606087690c
Bishoy Romany <7bishoyromany7@gmail.com>
2021-09-01 07:33:47 +0200

    INIT Apollo Server

---
+++ server/src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+require("dotenv").config();
+const { ApolloServer, gql } = require("apollo-server-express");
+const app = express();
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+async function startApolloServer() {
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.get("/", (req, res) => res.send("Hello World"));
+
+  app.listen({ port: PORT }, () =>
+    console.log(`Server running at http://localhost:${PORT}`)
+  );
+}
+
+startApolloServer();
---
+++ server/src/constants.js
@@ -0,0 +1,4 @@
+const ANIME_CHARACTERS_API =
+  "https://api.ranker.com/lists/518411/items?limit=40&offset=261";
+
+export { ANIME_CHARACTERS_API };
